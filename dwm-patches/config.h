9f2a4a0b0d47421813e2ba07d157ffa11486e3ea
Sean DuBois <sean@siobud.com>
2016-01-11 20:29:58 -0600

    Remove rules from dwm config.h, before the gimp/firefox defaults were used

--- dwm-patches/config.h
+++ dwm-patches/config.h
@@ -27,13 +27,6 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
