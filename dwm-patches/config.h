d0185249abcdfdee38c5dbfaf3a7937a6a2dbce1
Sean DuBois <duboisea@justin.tv>
2023-02-16 13:16:15 -0500

    Switch to Inconsolata

--- dwm-patches/config.h
+++ dwm-patches/config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Anonymous Pro:size=8" };
-static const char dmenufont[]       = "Anonymous Pro:size=8";
+static const char *fonts[]          = { "Inconsolata:size=8" };
+static const char dmenufont[]       = "Inconsolata:size=8";
 static const char col_black[]       = "#000000";
 static const char col_green[]       = "#006501";
 static const char col_white[]       = "#ffffff";
