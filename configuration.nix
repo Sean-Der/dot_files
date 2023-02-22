10ba3ff2416120c15578043dc989145e7ad3f63d
Sean DuBois <sean@siobud.com>
2023-02-22 01:41:41 -0500

    Source fzf

--- configuration.nix
+++ configuration.nix
@@ -56,6 +56,7 @@ in {
         PROMPT_COLOR="1;31m"
         ((UID)) && PROMPT_COLOR="1;32m"
         PS1="\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
+        source ~/.fzf.bash
       '';
     };
   };
