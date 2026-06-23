0ffae0afd1cdf8f7641fb4de1fe419cbac0c98de
Sean DuBois <sean@thinkingmachines.ai>
2026-06-23 14:11:07 -0400

    Run formatting on init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,10 @@
-vim.cmd.colorscheme '256_jungle'
+vim.cmd.colorscheme("256_jungle")
 
-vim.g.mapleader = ','
-vim.g.maplocalleader = ','
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 vim.opt.wrap = false
-vim.opt.mouse = ''
+vim.opt.mouse = ""
 
 -- Case-insensitive searching
 vim.opt.ignorecase = true
@@ -14,11 +14,11 @@ vim.opt.smartcase = true
 vim.opt.autochdir = true
 
 -- reserve git gutter
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Leaving modes in Emacs+EVIL feels right
-vim.keymap.set({'n', 'i', 'v', 'x', '!'}, '<C-g>', '<Esc>')
-vim.keymap.set('t', '<C-g>', '<C-\\><C-n>')
+vim.keymap.set({ "n", "i", "v", "x", "!" }, "<C-g>", "<Esc>")
+vim.keymap.set("t", "<C-g>", "<C-\\><C-n>")
 
 -- Don't yank on paste
 vim.keymap.set("x", "p", "P", { silent = true })
@@ -29,12 +29,12 @@ vim.pack.add({
 	"https://github.com/ibhagwan/fzf-lua",
 
 	"https://github.com/neovim/nvim-lspconfig",
-        "https://github.com/hrsh7th/cmp-nvim-lsp",
-        "https://github.com/hrsh7th/cmp-buffer",
-        "https://github.com/hrsh7th/cmp-path",
-        "https://github.com/hrsh7th/cmp-cmdline",
-        "https://github.com/hrsh7th/nvim-cmp",
-        "https://github.com/j-hui/fidget.nvim",
+	"https://github.com/hrsh7th/cmp-nvim-lsp",
+	"https://github.com/hrsh7th/cmp-buffer",
+	"https://github.com/hrsh7th/cmp-path",
+	"https://github.com/hrsh7th/cmp-cmdline",
+	"https://github.com/hrsh7th/nvim-cmp",
+	"https://github.com/j-hui/fidget.nvim",
 
 	"https://github.com/stevearc/conform.nvim",
 
@@ -42,22 +42,22 @@ vim.pack.add({
 })
 
 local signs = {
-    add          = { text = '+' },
-    change       = { text = '~' },
-    delete       = { text = '-' },
-    topdelete    = { text = '-' },
-    changedelete = { text = '~' },
-    untracked    = { text = '┆' },
-  }
-require('gitsigns').setup({signs = signs, signs_staged = signs})
+	add = { text = "+" },
+	change = { text = "~" },
+	delete = { text = "-" },
+	topdelete = { text = "-" },
+	changedelete = { text = "~" },
+	untracked = { text = "┆" },
+}
+require("gitsigns").setup({ signs = signs, signs_staged = signs })
 
-local fzf = require 'fzf-lua'
-fzf.setup({ keymap = { builtin = { true, ['<C-g>'] = 'hide' } } })
-vim.keymap.set('n', '<leader>g', fzf.live_grep)
-vim.keymap.set('n', '<leader>o', fzf.files)
-vim.keymap.set('n', '<leader>s', fzf.grep_cword)
-vim.keymap.set('n', '<leader>m', fzf.oldfiles)
-vim.keymap.set('n', '<leader>r', fzf.registers)
+local fzf = require("fzf-lua")
+fzf.setup({ keymap = { builtin = { true, ["<C-g>"] = "hide" } } })
+vim.keymap.set("n", "<leader>g", fzf.live_grep)
+vim.keymap.set("n", "<leader>o", fzf.files)
+vim.keymap.set("n", "<leader>s", fzf.grep_cword)
+vim.keymap.set("n", "<leader>m", fzf.oldfiles)
+vim.keymap.set("n", "<leader>r", fzf.registers)
 
 -- Disable LSP syntax hightlight
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -65,78 +65,83 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		client.server_capabilities.semanticTokensProvider = nil
 	end,
-});
-
-vim.lsp.config('gopls', {
-  capabilities = vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), require("cmp_nvim_lsp").default_capabilities()),
-  settings = {
-    gopls = {
-      analyses = {
-        shadowed = true,
-        unusedparams = true,
-      },
-      staticcheck = true,
-      gofumpt = true,
-    },
-  },
 })
-vim.lsp.enable('gopls')
+
+vim.lsp.config("gopls", {
+	capabilities = vim.tbl_deep_extend(
+		"force",
+		{},
+		vim.lsp.protocol.make_client_capabilities(),
+		require("cmp_nvim_lsp").default_capabilities()
+	),
+	settings = {
+		gopls = {
+			analyses = {
+				shadowed = true,
+				unusedparams = true,
+			},
+			staticcheck = true,
+			gofumpt = true,
+		},
+	},
+})
+vim.lsp.enable("gopls")
 
 require("fidget").setup({})
-local cmp = require('cmp')
+local cmp = require("cmp")
 cmp.setup({
-  completion = {
-    autocomplete = false,
-  },
-  sources = {
-    { name = 'path' },
-    { name = 'nvim_lsp' },
-    { name = 'buffer',  keyword_length = 2 },
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-n>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        cmp.mapping.complete()(fallback)
-        cmp.select_next_item({ count = 0 })
-      end
-    end,
-    ['<C-p>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        cmp.mapping.complete()(fallback)
-        cmp.select_prev_item({ count = 0 })
-      end
-    end,
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  }),
+	completion = {
+		autocomplete = false,
+	},
+	sources = {
+		{ name = "path" },
+		{ name = "nvim_lsp" },
+		{ name = "buffer", keyword_length = 2 },
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-n>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			else
+				cmp.mapping.complete()(fallback)
+				cmp.select_next_item({ count = 0 })
+			end
+		end,
+		["<C-p>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			else
+				cmp.mapping.complete()(fallback)
+				cmp.select_prev_item({ count = 0 })
+			end
+		end,
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	}),
 })
 
-require('conform').setup({
-  formatters_by_ft = {
-    go = { "goimports", "gofmt" },
-    python = { "ruff" , "isort"},
-    ['*'] = { 'trim_whitespace' },
-  },
+require("conform").setup({
+	formatters_by_ft = {
+		go = { "goimports", "gofmt" },
+		python = { "ruff", "isort" },
+		["*"] = { "trim_whitespace" },
+	},
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
 
 require("trouble").setup({
-  modes = {
-    diagnostics = {
-      auto_open = true,
-      auto_close = true,
-      filter = {
-        buf = 0,
-      },
-    },
-  },
+	modes = {
+		diagnostics = {
+			auto_open = true,
+			auto_close = true,
+			filter = {
+				buf = 0,
+			},
+		},
+	},
 })
